d037f9e34f513d746eef9ac9dce9995a78152701
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-11-30 13:59:00 -0500

    Finished filter ls

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -16,10 +16,22 @@
 
 //MY FIRST ASYNC I/O
 
-var fs = require('fs')
-var file = process.argv[2]
+// var fs = require('fs')
+// var file = process.argv[2]
 
-fs.readFile(file, function (err, contents) {
-var lines = contents.toString().split('\n').length - 1
-console.log(lines)
-})
+// fs.readFile(file, function (err, contents) {
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
+// })
+
+//FILTERED L/S
+
+// var fs = require('fs')
+// var path = require('path')
+
+// fs.readdir(process.argv[2], function (err, list) {
+// list.forEach(function (file) {
+// if (path.extname(file) === '.' + process.argv[3])
+// console.log(file)
+// })
+// })
