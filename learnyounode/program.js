349649f82bff86f9daedbcf93243550836fcefa6
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-11-30 13:56:13 -0500

    My first async I/O completed

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,4 +1,4 @@
-// Baby Steps
+// BABAY STEPS
 // var sum = 0
 
 // for (i=2; i < process.argv.length; i++){
@@ -8,8 +8,18 @@
 // console.log(sum);
 
 
-//My First I/O
+//MY FIRST I/O
+// var fs = require('fs')
+// var contents = fs.readFileSync(process.argv[2])
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
+
+//MY FIRST ASYNC I/O
+
 var fs = require('fs')
-var contents = fs.readFileSync(process.argv[2])
+var file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
 var lines = contents.toString().split('\n').length - 1
 console.log(lines)
+})
