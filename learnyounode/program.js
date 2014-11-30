35d712c9244d42f532322cd5d05ed8a9beffc3eb
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-11-30 13:51:43 -0500

    My first i/o finished

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,7 +1,15 @@
-var sum = 0
+// Baby Steps
+// var sum = 0
 
-for (i=2; i < process.argv.length; i++){
-	sum += Number(process.argv[i]);
-}
+// for (i=2; i < process.argv.length; i++){
+// 	sum += Number(process.argv[i]);
+// }
 
-console.log(sum);
+// console.log(sum);
+
+
+//My First I/O
+var fs = require('fs')
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines)
